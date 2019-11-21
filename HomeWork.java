bd4197a13f5097e77bfc49395f458041da5b0bb7
0632024 <55527022+0632024@users.noreply.github.com>
2019-11-21 16:26:12 +0800

    Create HomeWork.java

---
+++ HomeWork.java
@@ -0,0 +1,17 @@
+class CShapehw1
+{
+  public double area()
+  {
+  return 0.0;
+  }
+  public static void largest(CShapehw10_11 s[])  
+ {
+ shp[2]=new CTrianglehw10_11(7.0,8.0); 
+
+System.out.println("三角形面積="+shp[2].area());
+CShapehw10_11.largest(shp);//將陣列傳遞到largest()函數內比較大小  
+}
+}
+}
+
+http://sychen.nutc.edu.tw/index.php/curriculum/100/56-cc/461-ppp
